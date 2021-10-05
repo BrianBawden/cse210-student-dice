a43c25d748c44ce4a130b1f1d2eee58c8fc85185
Brian <brianbawden001@gmail.comgit config --global user.name Brianrungit config --global user.email brianbawden001@gmail.com>
2021-10-05 10:21:33 -0400

    Working on throw class

--- dice/game/thrower.py
+++ dice/game/thrower.py
@@ -1,3 +1,21 @@
 import random
 
 # TODO: Define the Thrower class here.
+
+class thrower:
+
+    def __init__(self):
+
+
+    # you will need function called 'throw_dice()'
+    def throw_dice(self):
+
+       self.rolls = random.sample(range(1,6), 5)
+
+       
+        
+    # you will need function called 'get_points()'
+
+    # you will need function called 'dice()'
+
+    # you will need function called 'can_throw()'
