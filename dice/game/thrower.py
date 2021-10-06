d5eb9ae87c30789efcf8369265cca6eab62c2b50
Brian <brianbawden001@gmail.comgit config --global user.name Brianrungit config --global user.email brianbawden001@gmail.com>
2021-10-05 21:44:46 -0400

    set up __init__ and throw_dice

--- dice/game/thrower.py
+++ dice/game/thrower.py
@@ -5,17 +5,33 @@ import random
 class thrower:
 
     def __init__(self):
+        
+        self.dice = []
 
 
     # you will need function called 'throw_dice()'
     def throw_dice(self):
 
-       self.rolls = random.sample(range(1,6), 5)
-
-       
+        self.dice.clear()
+        self.dice.append(random.sample(range(1,6), 5)) 
         
-    # you will need function called 'get_points()'
 
-    # you will need function called 'dice()'
+    # you will need function called 'get_points()'
+    def get_points(self):
+
+        points = 0
+
+        if self.rolls == 1:
+            points += 100
+        elif self.rolls == 5:
+            points += 50
+
+        return points
+        
 
     # you will need function called 'can_throw()'
+    def can_throw(self):
+
+        if 1 in self.rolls:
+
+    
